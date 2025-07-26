bdb6924f5d29215b10912541e7d295c8e8efef2f
staticxrjc <staticxrjc@gmail.com>
2025-07-26 16:29:16 -0700

    Final Solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
 int main() {
-    std::printf("Hello World!\n");
+    int input;
+    std::cin >> input;
 
+    if (input < 1 || input > 100) {
+        std::cout << "NO";
+        return 0;
+    }
+    if (input % 2 == 0 && input != 2)
+        std::cout << "YES";
+    else
+        std::cout << "NO";
     return 0;
 }
